b2994b230e136c4ff00cbbb3066638dc5da120f2
teerapon <the_trp@hotmail.com>
2023-04-10 15:35:30 +0700

    TEST OPS Mobile

--- crud_api/index.js
+++ crud_api/index.js
@@ -5,15 +5,6 @@ const cors = require('cors');
 const fileupload = require("express-fileupload");
 const bodyParser = require('body-parser');
 
-// const dotenv = require('dotenv');
-// const env = dotenv.config().parsed
-// const line = require('@line/bot-sdk');
-
-// const lineConfig = {
-//   channelAccessToken: env.ACCESSTOKEN,
-//   channelSecret: env.SECRET_TOKEN,
-// }
-
 const PTEC_USERSRIGHT_Routes = require('./routes/PTEC_USERSRIGHT_Routes');
 const PTEC_FA_Routes = require('./routes/PTEC_FA_Rotes');
 const PTEC_FA_PERIOD_Routes = require('./routes/PTEC_FA_PERIOD_Routes');
@@ -21,21 +12,18 @@ const TEST_PDPA_Routes = require('./routes/TEST_PDPA_Routes');
 const TEST_NewNTI_Routes = require('./routes/TEST_NewNTI_Routes');
 const PTEC_OPS_Mobile = require('./routes/PTEC_OPS_Mobile_Routes');
 
-
-const corsOptions = {
-    origin: 'http://example.com', // use your actual domain name (or localhost), using * is not recommended
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
-}
-
 const app = express();
+
+const corsConfig = {
+    credentials: true,
+    origin: true,
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("files"));
 app.use(fileupload());
-app.use(cors(corsOptions))
+app.use(cors(corsConfig))
 
 app.use('/api', PTEC_USERSRIGHT_Routes.routes);
 app.use('/api', PTEC_FA_Routes.routes);
