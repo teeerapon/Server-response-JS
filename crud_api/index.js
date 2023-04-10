2399bf7c87fa71ed4db54c74ca79192fed0db325
teerapon <the_trp@hotmail.com>
2023-04-10 12:24:57 +0700

    + - Fixed Cors

--- crud_api/index.js
+++ crud_api/index.js
@@ -22,29 +22,20 @@ const TEST_NewNTI_Routes = require('./routes/TEST_NewNTI_Routes');
 const PTEC_OPS_Mobile = require('./routes/PTEC_OPS_Mobile_Routes');
 
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
+const corsOptions = {
+    origin: 'http://example.com', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}
 
 const app = express();
 
-// app.post('/webhooks', line.middleware(lineConfig), async (req,res) => {
-//     try {
-//         const events = req.body.events
-//         console.log('event=>>>>', events)
-//         return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK")
-//     } catch (error) {
-//         res.status(500).end
-//     }
-// })
-
-app.use(bodyParser.urlencoded({ extended:  true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("files"));
 app.use(fileupload());
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 app.use('/api', PTEC_USERSRIGHT_Routes.routes);
 app.use('/api', PTEC_FA_Routes.routes);
@@ -53,4 +44,4 @@ app.use('/api', TEST_PDPA_Routes.routes);
 app.use('/api', TEST_NewNTI_Routes.routes);
 app.use('/api', PTEC_OPS_Mobile.routes);
 
-app.listen(config.PTEC.port, ()=> console.log('Server is listening on http://localhost : port : ' + config.PTEC.port));
+app.listen(config.PTEC.port, () => console.log('Server is listening on http://localhost : port : ' + config.PTEC.port));
